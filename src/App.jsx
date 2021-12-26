7e92762caa763db854a0384d0e7423ede37672f6
bapic <itsbapic@gmail.com>
2021-12-26 13:07:21 -0800

    Stage 4 complete + Node Vercel Fix

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import { useEffect, useMemo, useState } from "react";
 import { useWeb3 } from "@3rdweb/hooks";
 import { ThirdwebSDK } from "@3rdweb/sdk";
 import { ethers } from "ethers";
+import { UnsupportedChainIdError } from "@web3-react/core";
+
 
 // We instatiate the sdk on Rinkeby.
 const sdk = new ThirdwebSDK("rinkeby");
@@ -179,6 +181,20 @@ const App = () => {
       });
   }, [address]);
 
+
+  if (error instanceof UnsupportedChainIdError ) {
+    return (
+      <div className="unsupported-network">
+        <h2>Please connect to Rinkeby</h2>
+        <p>
+          This dapp only works on the Rinkeby network, please switch networks
+          in your connected wallet.
+        </p>
+      </div>
+    );
+  }
+
+  
   // This is the case where the user hasn't connected their wallet
   // to your web app. Let them call connectWallet.
   if (!address) {
